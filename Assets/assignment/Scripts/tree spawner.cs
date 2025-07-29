920dd0661b740b978e98c33a5be2e055cc9fb35d
Diego(Aria) <deejay.e2@icloud.com>
2025-07-29 15:38:32 -0400

    working on my assignment as well as coding gymn
    doing work on a coding gym as well as the assigmensts trying to figure out how to get my stupid tree spawner to work

--- Assets/assignment/Scripts/tree spawner.cs
+++ Assets/assignment/Scripts/tree spawner.cs
@@ -1,28 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class treespawner : MonoBehaviour
 {
+    public Slider FuelBar;
+    public float Maxfuel = 100;
+    public float minfuel;
+    public float fueltake;
+    public float currentfuel;
     public GameObject prefabToSpawn;
+    public float Treecount;
+    bool treespawn = false;
+    public float Timer = 0;
     // using this to actually get the trees to spawn
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentfuel = Maxfuel;
+        FuelBar.value = currentfuel / Maxfuel;
+        Treecount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool leftclick = Input.GetMouseButtonDown(0);
-        //this just tests if the left mouse button has been clicked so it knows to spawn a tree
-        if (leftclick)
-        {
+        Timer += Time.deltaTime;
+
+        if ( treespawn == true){
             GameObject spawnedtree = Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
             // this gets the tree and spawns it at teh position of the spawner 
             Destroy(spawnedtree, 10);
-           //this destroys the tree after 20 seconds i know that seems long but i had it at 10 and on lower frame rates it would tend to not finish before disappearing
+            treespawn = false;
         }
     }
+
+
+        
+    public void OnTreeSpawn()
+    {
+        if (currentfuel > minfuel)
+        {
+            treespawn = true;
+            currentfuel -= fueltake;
+            FuelBar.value = currentfuel / Maxfuel;
+            Treecount += 1;
+        }
+
+
+    }
+
+    public void OnRefuel()
+    {
+        currentfuel = Maxfuel;
+    }
+
 }
